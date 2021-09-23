ed344e27a586a495c8ccc60ca2f7b09e1db6b9c4
bagus2x <dutapanggulu@gmail.com>
2021-09-23 07:52:25 +0700

    fix wrong data type

--- src/views/History/components/InputDate.tsx
+++ src/views/History/components/InputDate.tsx
@@ -1,8 +1,6 @@
 import IconButton from '@material-ui/core/IconButton';
 import ClearIcon from '@material-ui/icons/Clear';
 import { DateTimePicker } from '@material-ui/pickers';
-import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
-import moment from 'moment';
 import React, { MouseEvent, useState } from 'react';
 import { useHistory } from 'react-router';
 import useQueryString from '../../../hooks/query-string';
@@ -14,21 +12,21 @@ interface InputDateProps {
   minDate: number;
 }
 
-const getNullDate = (stringUnix: string | null): MaterialUiPickersDate => {
+const getNullDate = (stringUnix: string | null) => {
   if (!stringUnix) return null;
   const numUnix = parseInt(stringUnix);
   if (!numUnix) return null;
-  return moment.unix(numUnix);
+  return new Date(numUnix * 1000);
 };
 
 const InputDate = ({ label, queryString: queryStr, minDate }: InputDateProps) => {
   const queryString = useQueryString();
-  const [startDate, setStartDate] = useState<MaterialUiPickersDate | null>(getNullDate(queryString.get(queryStr)));
+  const [startDate, setStartDate] = useState<Date | null>(getNullDate(queryString.get(queryStr)));
   const history = useHistory();
 
-  const handleStartDateChange = (date: MaterialUiPickersDate) => {
+  const handleStartDateChange = (date: any) => {
     setStartDate(date);
-    queryString.set(queryStr, (new Date(date.toString()).getTime() / 1000).toString());
+    queryString.set(queryStr, (new Date(date?.toString()).getTime() / 1000).toString());
     history.push('?' + queryString.toString());
   };
 
